bfa44494bea5d9f0582d115252e4f05b2adc4bcf
BenjaTorracoBenito <benjamintorracobenito@gmail.com>
2024-08-10 17:57:21 -0300

    asd

--- 00-my-brand/vite.config.js
+++ 00-my-brand/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
+// Cambia '/my-portfolio/' por el nombre de tu repositorio
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/my-portfolio/', // Ajusta esto según el nombre de tu repositorio
+  build: {
+    outDir: 'dist', // Esta es la carpeta de salida para los archivos construidos
+  },
+});
